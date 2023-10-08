b7a4a6b70a11de600aecaa89b58933afc9207f18
TroyRandall <troyrandall18@gmail.com>
2023-10-07 20:52:22 -0400

    updated navbar to not feel so plain

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -11,61 +11,69 @@ function Navigation({ isLoaded }) {
 
   return (
     isLoaded && (
-      <div id="nav-container">
-        {" "}
-        <div id="nav-logo-container" className="nav-flex-items">
-          <div id="nav-div-1">
-            {" "}
-            <NavLink exact to="/" id="nav-logo">
-              <img
-                src={logo}
-                alt="hands outlined in white clapping with a vibrant blue background"
-                id="nav-logo"
-              />
-            </NavLink>
-            <NavLink
-              exact
-              to="/spotlight"
-              id="nav-spotlight"
-              style={{ textDecoration: "none" }}
-            >
-              <h3>Spotlight</h3>
-            </NavLink>
+      <div class="navbar" id="nav-container">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+
+        <div class="nav-content">
+          <div id="nav-logo-container" className="nav-flex-items">
+            <div id="nav-div-1">
+              {" "}
+              <NavLink exact to="/" id="nav-logo">
+                <img
+                  src={logo}
+                  alt="hands outlined in white clapping with a vibrant blue background"
+                  id="nav-logo"
+                />
+              </NavLink>
+              <NavLink
+                exact
+                to="/spotlight"
+                id="nav-spotlight"
+                style={{ textDecoration: "none" }}
+              >
+                <h3>Spotlight</h3>
+              </NavLink>
+            </div>
           </div>
-        </div>
-        <NavLink exact to="/" className="nav-flex-items" id="nav-title-link">
-          <h1 id="nav-title" style={{ textDecoration: "none" }}>
-            Applause
-          </h1>
-        </NavLink>
-        <div id="Nav-buttons-container" className="nav-flex-items">
-          <div id='nav-div-2'>
-               {currentUser ? (
-            <>
-              <NavLink exact to={profileURL} >
-                <h3 style={{ textDecoration: "none" }}>
-                  Profile
-                </h3>
-
-              </NavLink>
-              <NavLink exact to="/events" >
-                <h3 style={{ textDecoration: "none" }}>Events</h3>
-
-              </NavLink>
-            </>
-          ) : (
-            <>
-              <NavLink exact to="/login" style={{ textDecoration: "none", color: 'black' }}>
-                Log in
-              </NavLink>
-              <NavLink exact to="/signup" style={{ textDecoration: "none", color: 'black' }}>
-                Sign Up
-              </NavLink>
-            </>
-          )}
-          <ProfileButton user={currentUser} />
+          <NavLink exact to="/" className="nav-flex-items" id="nav-title-link">
+            <h1 id="nav-title" style={{ textDecoration: "none" }}>
+              Applause
+            </h1>
+          </NavLink>
+          <div id="Nav-buttons-container" className="nav-flex-items">
+            <div id="nav-div-2">
+              {currentUser ? (
+                <>
+                  <NavLink exact to={profileURL}>
+                    <h3 style={{ textDecoration: "none" }}>Profile</h3>
+                  </NavLink>
+                  <NavLink exact to="/events">
+                    <h3 style={{ textDecoration: "none" }}>Events</h3>
+                  </NavLink>
+                </>
+              ) : (
+                <>
+                  <NavLink
+                    exact
+                    to="/login"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    Log in
+                  </NavLink>
+                  <NavLink
+                    exact
+                    to="/signup"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    Sign Up
+                  </NavLink>
+                </>
+              )}
+              <ProfileButton user={currentUser} />
+            </div>
           </div>
-
         </div>
       </div>
     )
