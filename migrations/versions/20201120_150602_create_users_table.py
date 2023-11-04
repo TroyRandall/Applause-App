d70738358edff15f031a86e24e86430267691106
TroyRandall <troyrandall18@gmail.com>
2023-11-03 21:24:27 -0400

    adjusted migrations to fit renders load pattern

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -32,9 +32,9 @@ def upgrade():
     sa.Column('aboutMe', sa.String(length=255), nullable=False),
     sa.Column('role', sa.String(), default='Fan'),
     sa.Column('imageUrl', sa.String(), nullable=True),
-    sa.Column('FBlink', sa.String(), nullable=True),
-    sa.Column('instaLink', sa.String(), nullable=True),
-    sa.Column('GHLink', sa.String(), nullable=True),
+    # sa.Column('FBlink', sa.String(), nullable=True),
+    # sa.Column('instaLink', sa.String(), nullable=True),
+    # sa.Column('GHLink', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
