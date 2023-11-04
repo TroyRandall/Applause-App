2bb7caffab0de28fa4ce56f279044c901b8b6e2d
TroyRandall <troyrandall18@gmail.com>
2023-11-03 21:46:51 -0400

    adjusted userPhotos migrations to account for ENV variables in production environment

--- migrations/versions/20231024_155121_.py
+++ migrations/versions/20231024_155121_.py
@@ -8,6 +8,10 @@ Create Date: 2023-10-24 15:51:21.974267
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = '9217fb77b574'
